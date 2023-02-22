fcf9d9cffa1c425a84834136b1f9f1cd8f74152d
Marcat-A <anthony.marcato@gmail.com>
2023-02-22 17:12:12 +0100

    home page ok

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import css from "./Card.module.css";
 
 const Card = ({ logement }) => {
-  return <div>{logement.title}</div>;
+  console.log(logement);
+  return (
+    <div
+      className={css.container}
+      style={{
+        background: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%),url(${logement.cover})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <h2 className={css.title}>{logement.title}</h2>
+    </div>
+  );
 };
 
 export default Card;
