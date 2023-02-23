923f83b3b91888ae5353beea693ab694f7428fda
Marcat-A <anthony.marcato@gmail.com>
2023-02-23 16:43:23 +0100

    not found ok

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import css from "./NotFound.module.css";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>404</h1>
+      <h2 className={css.secondTitle}>
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link to="/" className={css.link}>
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
